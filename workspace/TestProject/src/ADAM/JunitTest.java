1608ce36e80b62a8865e699c21e033bb7872790e
abenzelt <abenzelt@LFR000967.corp.capgemini.com>
2017-04-28 12:44:30 +0200

    first commit

---
+++ workspace/TestProject/src/ADAM/JunitTest.java
@@ -0,0 +1,30 @@
+package ADAM;
+
+/**
+ * @author abenzelt
+ *
+ */
+public class JunitTest {
+
+	public int square(int x) {
+
+		return x * x;
+
+	}
+
+	public int countA(String word) {
+		int count = 0;
+		for (int i = 0; i < word.length(); i++) {
+
+			if (word.charAt(i) == ('a') || word.charAt(i) == ('A')) {
+
+				count++;
+			}
+
+		}
+
+		return count;
+
+	}
+
+}
